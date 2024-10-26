41885828f7de94ccd62b0ecaff267076308096f3
Jacintat30 <142204708+Jacintat30@users.noreply.github.com>
2024-10-26 15:48:40 -0500

    2nd/3rd commit
    Deleted 2 unnecessary from src and added more

---
+++ src/WeatherForecast.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
+
+  
+    function handleForecastResponse(response) {
+      setForecast(response.data.daily);
+      setLoaded(true);
+    }
+
+      if (loaded) {
+      console.log(forecast);
+      return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+
+  } else {
+    let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleForecastResponse);
+  
+    return null;
+  }
+}
